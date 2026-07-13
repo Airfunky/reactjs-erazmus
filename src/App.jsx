7f8e2385ff95f3bfe2a159015bca4e2739243600
Lorenzo <lorenzo8banachewicz@gmail.com>
2026-07-13 11:28:05 +0200

    Slice usage in arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 function App(){
-  const students = ["Rahim", "Karim", "Jamal", "bob"];
+  const students = [
+    "Rahim",
+    "Karim",
+    "Jamal",
+    "bob",
+    "ariful",
+    "ari",
+    "arivederci"
+        ];
 
+    
   return(
     <div>
-      {students.map((student) => (
-        <h2>{student}</h2>
+      {students.slice(3, 4).map((student, index) => (
+        <h2 key={index}> {student}</h2>
       ))}
     </div>
-   
-  );
-   
+  );  
 }
 
 export default App;
