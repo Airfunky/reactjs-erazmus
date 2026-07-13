555a8c2cd2230c18cab9082ff6b922b45080bcc2
Lorenzo <lorenzo8banachewicz@gmail.com>
2026-07-13 11:43:43 +0200

    Hiding specific names, !==

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,15 @@ function App(){
     "arivederci"
         ];
 
-    const showStudents = ["Rahim", "Jamal"];
+    
   return(
     <div>
       {students
-          .filter((student) => showStudents.includes(student))
-          .map((student, index) => (
-            <h2 key={index}>{student}</h2>
-          ))}
+          .filter((student) => student !=="Rahim" && student !== 
+      "Hasan")
+        .map((student, index) => (
+          <h2 key={index}> {student} </h2>
+        ))}
     </div>
   );  
 }
