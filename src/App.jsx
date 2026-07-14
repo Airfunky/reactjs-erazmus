1faea4ec868b5bbc2ef9eb9e778f438b1e064e8c
Lorenzo <lorenzo8banachewicz@gmail.com>
2026-07-14 13:05:06 +0200

    Conditional Method 2, The Ternary Operator ?:

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 function App() {
  
-const isLogin = true;
+const isLogin = false;
 
-if (isLogin){
-  return <h1>Welcome Back!</h1>;
-}
-
-  return <h1>Please Login</h1>;
+return(
+  <div>
+    {isLogin ? <h1>Welcome Back</h1> : <h1>Please Login</h1> }
+  </div>
+)
    
 }
 
